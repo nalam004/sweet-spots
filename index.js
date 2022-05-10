674da55e11b4c23ddf1ce3cf1c10931d3dfb20a8
Niharika Alam <niharikaalam1234@gmail.com>
2022-05-09 20:46:27 -0400

    find bakeries near user

---
+++ index.js
@@ -0,0 +1,156 @@
+const apiKey = "AAPKf3c7596b921c4f4ba2b0e154a06df0cezPiZRCtAZHP5l0ClIf_nW0s-BCgd0q1drCVC0JAcduiwUv_DoOyTVY4OlkCs7Nl6";
+const authentication = arcgisRest.ApiKeyManager.fromKey(apiKey);
+let bounds = new mapboxgl.LngLatBounds();
+const basemapEnum = "ArcGIS:Navigation";
+let coordinates;
+let map;
+let popup;
+
+if ("geolocation" in navigator) { 
+    navigator.geolocation.getCurrentPosition(position => { 
+        coordinates = [position.coords.longitude, position.coords.latitude];
+
+        mapboxgl.accessToken = 'pk.eyJ1IjoibmFsYW0wMDMiLCJhIjoiY2tpczQ5MGZ1MDdueTJ4cGo5cHBlbTB6OSJ9.dISsU0VNJu0-RCItgMXsig';
+
+        map = new mapboxgl.Map({
+            container: "map",
+            style: 'mapbox://styles/nalam003/cl2zerzar001315o53663qkrx',
+            center: coordinates,
+            zoom: 13
+        });
+
+        map.once("load", () => { 
+            // load marker image and user icon image
+            map.loadImage('https://cdn-icons-png.flaticon.com/512/619/619483.png', (error, image) => {
+                if (error) throw error;
+                map.addImage('cupcake', image);
+            })
+
+            map.loadImage('https://cdn.emojidex.com/emoji/seal/yum%28pudding%29.png?1455290868', (error, image) => {
+                if (error) throw error;
+                map.addImage('user-icon', image);
+            })
+
+            // add user location to map
+            map.addSource('user', {
+                type: 'geojson',
+                data: {
+                    type: 'FeatureCollection',
+                    features: [{
+                        type: 'Feature',
+                        geometry: {
+                            type: 'Point',
+                            coordinates: coordinates
+                        }
+                    }]
+                }
+            });
+
+            map.addLayer({
+                id: "user-marker",
+                source: "user",
+                type: "symbol",
+                layout: {
+                    'icon-image': 'user-icon', 
+                    'icon-size': 0.1
+                }
+            });
+            
+            // add places markers to map
+            map.addSource("places", {
+                type: "geojson",
+                data: {
+                    type: "FeatureCollection",
+                    features: []
+                }
+            });
+
+            map.addLayer({
+                id: "markers",
+                source: "places",
+                type: 'symbol',
+                layout: {
+                    'icon-image': 'cupcake', 
+                    'icon-size': 0.05
+                }
+            });
+
+            map.on("mouseenter", "markers", (e) => {
+                const place = e.features[0];
+                popup = new mapboxgl.Popup({ closeButton: false, closeOnClick: false })
+                                .setHTML(`<b>${place.properties.PlaceName}</b>`)
+                                .setLngLat(place.geometry.coordinates)
+                                .addTo(map);
+            });
+            
+            map.on("mouseleave", "markers", () => {
+                popup.remove();
+            });
+
+            map.on("click", "markers", (e) => {
+                const place = e.features[0];
+                let start = coordinates;
+                let end = place.geometry.coordinates;
+                updateRoute(start, end);
+            });
+
+            showPlaces();
+            addRouteLayer();
+        });
+    }); 
+} else { console.log("geolocation IS NOT availabe"); }
+
+function showPlaces() {
+    const category = "bakery";
+    if (!category) { return; }
+    
+    arcgisRest.geocode({
+        authentication,
+        outFields: "Place_addr,PlaceName",
+        
+        params: {
+            category,
+            location: map.getCenter().toArray().join(","),
+            maxLocations: 20
+        }
+    }).then((response) => {
+        map.getSource("places").setData(response.geoJson);
+        /*
+        response.geoJson.features.forEach(feature => {
+            bounds.extend(feature.geometry.coordinates);
+        });
+        map.fitBounds(bounds); */
+    }).catch((error) => { alert("There was a problem using the geocoder. See the console for details."); });
+}
+
+function addRouteLayer() {
+    map.addSource("route", {
+        type: "geojson",
+        data: {
+            type: "FeatureCollection",
+            features: []
+        }
+    });
+
+    map.addLayer({
+        id: "route-line",
+        type: "line",
+        source: "route",
+
+        paint: {
+            "line-color": "#ff69af",
+            "line-width": 4,
+            "line-opacity": 0.6
+        }
+    });
+}
+
+function updateRoute(start, end) {
+    arcgisRest.solveRoute({
+        stops: [start, end],
+        endpoint: "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World/solve",
+        authentication
+    }).then((response) => {
+        map.getSource("route").setData(response.routes.geoJson);
+    }).catch((error) => { alert("There was a problem using the route service. See the console for details."); });
+}
